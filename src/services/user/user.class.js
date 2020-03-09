2a6883413f91609337598d3bd229510403c662f4
Maurice Oegerli <maurice@oeger.li>
2020-03-09 22:04:16 +0100

    try override

--- src/services/user/user.class.js
+++ src/services/user/user.class.js
@@ -4,15 +4,26 @@ export class User extends nedb.Service {
   setup(app) {
     this.app = app;
   }
+
+  // constructor(options, app) {
+  //   this.model = options.Model
+  // }
+
   async create(data) {
     const Captcha = this.app.service('captcha')
     const captchaVerification = await Captcha.create({ token: data.captchaToken, solution: data.captchaSolution })
     if (captchaVerification.success) {
-      const user = await this._create(data)
-      return {
-        username: user.username,
-        success: true
-      }
+      this.model.insert(data, (err, doc) => {
+        if (err) {
+          throw new Error('Error registering user')
+        }
+        if (doc) {
+          return {
+            username: user.username,
+            success: true
+          }
+        }
+      })
     }
   }
 }
