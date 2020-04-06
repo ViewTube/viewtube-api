212f3646889da2a3474e6630cf45662db4a4b371
Maurice Oegerli <maurice@oeger.li>
2020-04-06 23:59:46 +0200

    🎉 nest api initial

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,24 @@
-export default {
-  env: {
-    es6: true,
-    node: true
-  },
-  extends: [
-    'standard'
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
+module.exports = {
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module'
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
   },
   rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
-  ignore: [
-    'test'
-  ]
-}
+};
