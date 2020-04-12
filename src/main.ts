5c943da4b32f79b8dfd4ad0835558df4b71d3d7c
Maurice Oegerli <maurice@oeger.li>
2020-04-12 10:52:44 +0200

    🔒 enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors()
   await app.listen(3030);
 }
 bootstrap();
