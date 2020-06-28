b939d870f9976ef9282b823e19bebdf37e6532af
Maurice Oegerli <maurice@oeger.li>
2020-06-28 22:32:35 +0200

    update videoplayback

--- src/youtube/videoplayback/videoplayback.service.ts
+++ src/youtube/videoplayback/videoplayback.service.ts
@@ -5,16 +5,16 @@ import path from 'path';
 import ffmpeg from 'fluent-ffmpeg';
 import { Readable } from 'stream';
 import { Response } from 'express';
+import splitFileStream from 'split-file-stream';
 
 @Injectable()
 export class VideoplaybackService {
-  private combinedOutput: string = path.resolve('output/output.mp4');
   private video: Readable;
 
   private async streamVideo(audioOutput: string, url: string, res: Response) {
     this.video = ytdl(url, {
       filter: (format) => format.container === 'mp4' && !format.audioBitrate,
-    }).on('info', (videoInfo, videoFormat: ytdl.videoFormat) => {
+    }).on('info', (_, videoFormat: ytdl.videoFormat) => {
       // console.log('Video info:\n', videoInfo);
       console.log('Video format:\n', videoFormat);
       res.writeHead(200, {
@@ -29,9 +29,7 @@ export class VideoplaybackService {
         // .save(this.combinedOutput)
         // .concat(res, { end: true })
         .videoCodec('libx264')
-        .on('progress', (chunkLength, downloaded, total) =>
-          this.logProgress(chunkLength, downloaded, total, 'video'),
-        )
+        .on('progress', (videoProgress) => console.log(videoProgress))
         .on('end', () => {
           fs.unlink(audioOutput, (err) => {
             if (err) {
@@ -51,24 +49,35 @@ export class VideoplaybackService {
   }
 
   async getVideoStream(url: string, res: Response) {
-    const audioOutput: string = path.resolve('output/sound-' + url + '.mp4');
-    let streamStarted = false;
+    const audioOutput: string = path.resolve(`output/sound-${url}.mp4`);
+    const audioFolder: string = path.resolve(`output/sound-${url}`);
+    const rangeStart = 0;
+    // if (fs.existsSync(audioOutput)) {
+    //   console.log('audio exists');
+    //   const stats = fs.statSync(audioOutput);
+    //   rangeStart = stats.size;
+    // }
     const audioStream = ytdl(url, {
+      quality: 'lowestaudio',
+      range: { start: rangeStart },
       filter: (format) => format.container === 'mp4' && !format.qualityLabel,
     })
+      .on('info', (_, videoFormat: ytdl.videoFormat) => {
+        console.log(videoFormat);
+      })
       .on('error', console.error)
       .on('progress', (chunkLength, downloaded, total) => {
         this.logProgress(chunkLength, downloaded, total, 'audio');
-        if (downloaded / total > 0.4 && streamStarted === false) {
-          streamStarted = true;
-          this.streamVideo(audioOutput, url, res);
-        }
+      })
+      .on('finish', () => {
+        console.log('done with audio');
+        this.streamVideo(audioOutput, url, res);
       })
       .pipe(fs.createWriteStream(audioOutput));
   }
 
   logProgress(chunkLength, downloaded, total, string) {
-    console.log(chunkLength, downloaded, total, string);
+    // console.log(chunkLength, downloaded, total, string);
     console.log(
       `${string}:${chunkLength}`,
       `${((downloaded / total) * 100).toFixed(2)}%`,
